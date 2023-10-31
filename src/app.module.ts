43786756c8ad58777ed41035cfc0e306afb29fa3
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-10-31 20:33:28 -0300

    feat: added new resource for cars on nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), CarsModule],
   controllers: [AppController],
   providers: [AppService],
 })
