73c98cd70e6264883c90ab6e7c50fa229dd9f52b
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-11-17 19:53:15 -0300

    chore: added new resource for cars

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { DatabaseModule } from './database/database.module';
+import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, CarsModule],
 })
 export class AppModule {}
