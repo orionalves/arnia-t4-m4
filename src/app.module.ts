751a8fdf8c5d66f4b296d5996e12353902a6de0d
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-11-23 08:56:26 -0300

    feat: added guards to protect routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     AuthModule,
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     CarsModule,
     UsersModule,
--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -10,32 +10,38 @@ import {
   Patch,
   Post,
   Query,
+  Request,
+  UseGuards,
 } from '@nestjs/common';
 
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
 import { UpdateCarDto } from './dto/update-car.dto';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('cars')
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   async create(@Body() payload: CreateCarDto) {
     return await this.carsService.create(payload);
   }
 
   @Get()
-  async find(@Query('brand') brand?: string) {
+  async find(@Request() req: Request, @Query('brand') brand?: string) {
     return await this.carsService.find(brand);
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   async show(@Param('id', ParseIntPipe) id: number) {
     return await this.carsService.show(id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard)
   async update(
     @Param('id', ParseIntPipe) id: number,
     @Body() payload: UpdateCarDto,
@@ -44,6 +50,7 @@ export class CarsController {
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
   @HttpCode(HttpStatus.NO_CONTENT)
   async delete(@Param('id', ParseIntPipe) id: number) {
     return await this.carsService.delete(id);
