cc8c5bbba794252da68b1da9d847856eca68c997
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2024-01-02 20:32:30 -0300

    feat: added new route for login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,25 @@
-import { BadRequestException, HttpException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 
 import { UserEntity } from '../database/entities';
 import { Repository } from 'typeorm';
 import { RegisterUserDto } from './dtos/register-user.dto';
+import { LoginDto } from './dtos/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserEntity)
     private usersRepository: Repository<UserEntity>,
+
+    private jwtService: JwtService,
   ) {}
 
   async register(payload: RegisterUserDto) {
@@ -27,6 +37,52 @@ export class AuthService {
       await this.usersRepository.save(newUser);
 
       return newUser;
+    } catch (error) {
+      console.log(error);
+
+      throw new HttpException(error.message, error.status);
+    }
+  }
+
+  async login(payload: LoginDto) {
+    try {
+      const { email, password } = payload;
+
+      const userToLogin = await this.usersRepository.findOne({
+        where: { email },
+        select: {
+          id: true,
+          email: true,
+          password: true,
+          role: true,
+        },
+      });
+
+      if (!userToLogin) {
+        throw new UnauthorizedException('Email or password wrong.');
+      }
+
+      const passwordMatch = await bcrypt.compare(
+        password,
+        userToLogin.password,
+      );
+
+      if (!passwordMatch) {
+        throw new UnauthorizedException('Email or password wrong.');
+      }
+
+      const tokenPayload = {
+        iss: 'edusync',
+        sub: 'auth',
+        aud: 'users',
+        id: userToLogin.id,
+        email: userToLogin.email,
+        role: userToLogin.role,
+      };
+
+      return {
+        token: await this.jwtService.signAsync(tokenPayload),
+      };
     } catch (error) {
       console.log(error);
 
