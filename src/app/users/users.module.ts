65d7fd0447d598799fe5a838105d5043a1e6e11c
Vitor Reis <vitor.reis@izap.com.br>
2024-01-03 22:05:27 -0300

    feat: class 02/01/24

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,9 +1,13 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { User } from 'src/databases/entities/User.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
