257ff1271d6d31be5dea41e7de77d33c4ca2b40a
Vitor Reis <vitor.reis@izap.com.br>
2024-01-16 15:03:36 -0300

    feat: class 16/01

--- src/app/auth/auth.controller.spec.ts
+++ src/app/auth/auth.controller.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { userRegisterDtoMock } from '../../testing/auth/user-register.dto.mock';
+import { userMock } from '../../testing/users/users.mock';
+import { userLoginDtoMock } from '../../testing/auth/user-login.dto.mock';
+import { tokenMock } from '../../testing/auth/token.mock';
+import { authServiceMock } from '../../testing/auth/auth-service.mock';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -8,7 +12,7 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [authServiceMock],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -16,5 +20,21 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('register', () => {
+    it('Should return a registered user', async () => {
+      const result = await controller.register(userRegisterDtoMock);
+
+      expect(result).toEqual(userMock);
+    });
+  });
+
+  describe('login', () => {
+    it('Should return a user access token', async () => {
+      const result = await controller.login(userLoginDtoMock);
+
+      expect(result).toEqual(tokenMock);
+    });
   });
 });
