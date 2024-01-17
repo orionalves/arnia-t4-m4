0d86d19d29c0df3fac5a8e7c1b086ccf25cf9947
Vitor Reis <vitor.reis@izap.com.br>
2024-01-16 22:28:26 -0300

    feat: class 16/01

--- src/app/users/users.controller.spec.ts
+++ src/app/users/users.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { userServiceMock } from '../../testing/users/user-service.mock';
+import { userMock, usersMock } from '../../testing/users/users.mock';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -8,7 +9,7 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [userServiceMock],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -16,5 +17,21 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('Should return a list of users', async () => {
+      const result = await controller.findAll();
+
+      expect(result).toEqual(usersMock);
+    });
+  });
+
+  describe('findOne', () => {
+    it('Should return a user', async () => {
+      const result = await controller.findOne('1');
+
+      expect(result).toEqual(userMock);
+    });
   });
 });
--- src/testing/users/user-service.mock.ts
+++ src/testing/users/user-service.mock.ts
@@ -1,9 +1,11 @@
 import { UsersService } from '../../app/users/users.service';
-import { userMock } from './users.mock';
+import { userMock, usersMock } from './users.mock';
 
 export const userServiceMock = {
   provide: UsersService,
   useValue: {
     findUserByEmail: jest.fn().mockResolvedValue(userMock),
+    findAll: jest.fn().mockResolvedValue(usersMock),
+    findOne: jest.fn().mockResolvedValue(userMock),
   },
 };
