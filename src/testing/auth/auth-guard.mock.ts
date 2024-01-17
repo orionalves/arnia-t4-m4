e97a3634e11550fe861c541f0e11ae868c3ebc39
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2024-01-17 20:24:44 -0300

    test: added tests e2e for users

--- src/testing/auth/auth-guard.mock.ts
+++ src/testing/auth/auth-guard.mock.ts
@@ -1,5 +1,13 @@
-import { CanActivate } from '@nestjs/common';
+import { CanActivate, ExecutionContext } from '@nestjs/common';
+
+import { RoleEnum } from '../../enums/role.enum';
 
 export const authGuardMock: CanActivate = {
-  canActivate: jest.fn(),
+  canActivate: jest.fn((context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+
+    request['user'] = { id: 1, role: RoleEnum.admin };
+
+    return true;
+  }),
 };
